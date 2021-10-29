3f98c390d01bc3bbf2cb7af92896cc1cfd3b4fb9
Tom M. Ragonneau <tom.ragonneau@connect.polyu.hk>
2021-10-29 09:49:53 +0800

    Initial code coverage process

---
+++ build_tools/github/coverage.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -x
+set -e
+
+# Create and activate a virtual environment
+python -m venv venv
+source venv/bin/activate
+
+# Install the dependencies and generate coverage report
+python -m pip install --progress-bar=off pytest pytest-cov
+python -m pytest --cov=./ --cov-report=xml
