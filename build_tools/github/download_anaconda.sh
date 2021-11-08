57745c5de56ce3bbe6b887e17c5db78c6a337226
Tom M. Ragonneau <tom.ragonneau@connect.polyu.hk>
2021-11-08 19:00:36 +0800

    Action for uploading package to PyPI

---
+++ build_tools/github/download_anaconda.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+ANACONDA_ORG="ragonneau"
+COBYQA_URL="https://pypi.anaconda.org/$ANACONDA_ORG/simple/cobyqa/"
+python -m pip install --progress-bar=off wheelhouse_uploader
+python -m wheelhouse_uploader fetch --version "$COBYQA_VERSION" --local-folder dist/ cobyqa "$COBYQA_URL"
