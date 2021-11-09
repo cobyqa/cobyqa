132819c691972822b7bc6bf340cba944fb38ad3c
Tom M. Ragonneau <tom.ragonneau@connect.polyu.hk>
2021-11-09 08:03:24 +0800

    Build unique wheel distribution

--- build_tools/github/test_source.sh
+++ build_tools/github/test_source.sh
@@ -9,7 +9,8 @@ python -m venv test_env
 source test_env/bin/activate
 
 # Install and test the source distribution
-python -m pip install --progress-bar=off cobyqa/cobyqa/dist/*.tar.gz
+cd cobyqa/cobyqa
+python -m pip install --progress-bar=off dist/*.tar.gz
 python -m pip install --progress-bar=off pytest
 python -c "import cobyqa; cobyqa.show_versions()"
 python -m pytest --pyargs cobyqa
