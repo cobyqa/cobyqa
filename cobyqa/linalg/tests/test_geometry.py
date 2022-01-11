Correct precision depending on bitness

cobyqa/linalg/tests/test_geometry.py
@@ -3,6 +3,7 @@ import pytest
 from numpy.testing import assert_, assert_raises
 
 from cobyqa.linalg import bvcs, bvlag
+from cobyqa.linalg.utils import get_bdtol
 from cobyqa.tests import assert_array_less_equal, assert_dtype_equal
 
 
@@ -31,9 +32,7 @@ class TestBVCS:
         assert_(isinstance(cauchy, float))
 
         # Ensure the feasibility of the output.
-        eps = np.finfo(float).eps
-        tol = 10.0 * eps * n
-        bdtol = tol * np.max(np.abs(np.r_[xl, xu]), initial=1.0)
+        bdtol = get_bdtol(xl, xu)
         assert_array_less_equal(xl - xpt[kopt, :] - step, bdtol)
         assert_array_less_equal(xpt[kopt, :] + step - xu, bdtol)
         assert_(np.linalg.norm(step) - delta <= bdtol)
@@ -76,9 +75,7 @@ class TestBVLAG:
         assert_(step.size == n)
 
         # Ensure the feasibility of the output.
-        eps = np.finfo(float).eps
-        tol = 10.0 * eps * n
-        bdtol = tol * np.max(np.abs(np.r_[xl, xu]), initial=1.0)
+        bdtol = get_bdtol(xl, xu)
         assert_array_less_equal(xl - xpt[kopt, :] - step, bdtol)
         assert_array_less_equal(xpt[kopt, :] + step - xu, bdtol)
         assert_(np.linalg.norm(step) - delta <= bdtol)

cobyqa/linalg/tests/test_optimize.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pytest
-from numpy.testing import assert_, assert_allclose, assert_raises
+from numpy.testing import assert_, assert_raises
 
 from cobyqa.linalg import bvtcg, cpqp, lctcg, nnls
-from cobyqa.linalg.utils import get_bdtol
+from cobyqa.linalg.utils import get_bdtol, get_lctol
 from cobyqa.tests import assert_array_less_equal, assert_dtype_equal
 
 
@@ -134,10 +134,8 @@ class TestLCTCG:
         assert_(step.size == n)
 
         # Ensure the feasibility of the output.
-        eps = np.finfo(float).eps
-        tol = 10.0 * eps * n
         bdtol = get_bdtol(xl, xu)
-        lctol = tol * np.max(np.abs(bub), initial=1.0)
+        lctol = get_lctol(Aub, bub)
         assert_array_less_equal(xl - x0 - step, bdtol)
         assert_array_less_equal(x0 + step - xu, bdtol)
         if mlub > 0:
@@ -202,8 +200,7 @@ class TestNNLS:
         assert_(x.size == n)
 
         # Ensure that the KKT conditions approximately hold at the solution.
-        eps = np.finfo(float).eps
-        lctol = 100.0 * eps * max(n, m) * np.max(np.abs(b), initial=1.0)
+        lctol = 10. * get_lctol(A, b)
         grad = np.dot(A.T, np.dot(A, x) - b)
         assert_array_less_equal(np.abs(grad[k:]), lctol)
         assert_array_less_equal(np.abs(grad[:k] * x[:k]), lctol)

cobyqa/linalg/utils.py
@@ -1,3 +1,5 @@
+import struct
+
 import numpy as np
 from scipy.linalg import get_blas_funcs
 
@@ -83,7 +85,10 @@ def get_bdtol(xl, xu, **kwargs):
     xl = np.asarray(xl)
     xu = np.asarray(xu)
 
-    eps = np.finfo(float).eps
+    if struct.calcsize('P') == 8:
+        eps = np.finfo(np.float64).eps
+    else:
+        eps = np.finfo(np.float32).eps
     tol = 10.0 * eps * xl.size
     temp = np.nan_to_num(np.abs(np.r_[xl, xu]), nan=1.0, posinf=1.0)
     bdtol = tol * np.max(temp, initial=1.0)
@@ -114,7 +119,10 @@ def get_lctol(A, b, **kwargs):
     A = np.asarray(A)
     b = np.asarray(b)
 
-    eps = np.finfo(float).eps
+    if struct.calcsize('P') == 8:
+        eps = np.finfo(np.float64).eps
+    else:
+        eps = np.finfo(np.float32).eps
     tol = 10.0 * eps * max(A.shape)
     lctol = tol * np.max(np.abs(b), initial=1.0)
     return kwargs.get('lctol', lctol)