aab6e42025bc54188b63353734f295a0db7149d4
Tom M. Ragonneau <t.ragonneau@gmail.com>
2022-04-11 13:25:08 +0800

    Force python3 during build

--- build_tools/github/download_anaconda.sh
+++ build_tools/github/download_anaconda.sh
@@ -5,5 +5,5 @@ set -x
 
 # Fetch the distributions uploaded to Anaconda
 COBYQA_URL="https://pypi.anaconda.org/ragonneau/simple/cobyqa/"
-python -m pip install --progress-bar=off wheelhouse_uploader
-python -m wheelhouse_uploader fetch --version "$COBYQA_VERSION" --local-folder dist/ cobyqa "$COBYQA_URL"
+python3 -m pip install --progress-bar=off wheelhouse_uploader
+python3 -m wheelhouse_uploader fetch --version "$COBYQA_VERSION" --local-folder dist/ cobyqa "$COBYQA_URL"
