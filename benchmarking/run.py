ad60c7dca56ef863276d536b71cbbcf7d46a8ad9
Tom M. Ragonneau <t.ragonneau@gmail.com>
2022-10-14 16:51:45 +0800

    Add initial benchmarking scripts

--- benchmarking/run.py
+++ benchmarking/run.py
@@ -1,5 +1,32 @@
+import argparse
+
 from profiles import Profiles
 
 if __name__ == '__main__':
-    profiles = Profiles(1, 2, 'unconstrained')
+    parser = argparse.ArgumentParser(description='Run the performance and data profile comparisons.')
+    parser.add_argument('--cf-old', default=False, type=bool, help='compare with the penultimate version')
+    args = parser.parse_args()
+
+    profiles = Profiles(1, 10, 'unconstrained')
     profiles(['cobyqa', 'newuoa'])
+    if args.cf_old:
+        profiles(['cobyqa', 'cobyqa-old'])
+    del profiles
+
+    profiles = Profiles(1, 10, 'bound')
+    profiles(['cobyqa', 'bobyqa'])
+    if args.cf_old:
+        profiles(['cobyqa', 'cobyqa-old'])
+    del profiles
+
+    profiles = Profiles(1, 10, 'adjacency linear')
+    profiles(['cobyqa', 'lincoa'])
+    if args.cf_old:
+        profiles(['cobyqa', 'cobyqa-old'])
+    del profiles
+
+    profiles = Profiles(1, 10, 'quadratic other')
+    profiles(['cobyqa', 'cobyla'])
+    if args.cf_old:
+        profiles(['cobyqa', 'cobyqa-old'])
+    del profiles
