9383e45e47dd1539ef301ddf4546132f5b7076a8
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-09-15 16:25:36 +0800

    Improve feasibility.py

--- examples/feasibility.py
+++ examples/feasibility.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
-"""
-Solve the feasibility problem sum(sin(x)) = sum(cos(x)) = 0 in 5 variables.
+r"""
+Solve the feasibility problem
+
+.. math::
+
+    (y + x^2)^2 + 0.1 y^2 \le 1,\\
+    y \le e^{-x} - 3,\\
+    y \le x - 4.
+
 """
 import numpy as np
 
 from cobyqa import minimize
 
 
-def sum_trig(x):
-    return [np.sum(np.sin(x)), np.sum(np.cos(x))]
+def cub(x):
+    return [(x[1] + x[0] ** 2) ** 2 + 0.1 * x[1] ** 2 - 1, x[1] - np.exp(-x[0]) + 3]
 
 
 if __name__ == '__main__':
+    aub = [[-1.0, 1.0]]
+    bub = [-4.0]
+
     options = {'verbose': True}
-    minimize(None, np.zeros(5), ceq=sum_trig, options=options)
+    minimize(None, np.zeros(2), aub=aub, bub=bub, cub=cub, options=options)
