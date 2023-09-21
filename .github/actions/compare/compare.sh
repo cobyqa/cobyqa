0bcbaf07a30a61a5b2ad5e76831a8743c65c2f7e
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-09-21 12:24:52 +0800

    Run all scripts

--- .github/actions/compare/compare.sh
+++ .github/actions/compare/compare.sh
@@ -6,3 +6,6 @@ set -x
 python -m pip install --progress-bar=off cobyqa
 cd benchmarks
 python unconstrained.py
+python bound_constrained.py
+python linearly_constrained.py
+python nonlinearly_constrained.py
--- benchmarks/unconstrained.py
+++ benchmarks/unconstrained.py
@@ -2,4 +2,4 @@ from profiles import Profiles
 
 if __name__ == '__main__':
     profiles = Profiles(1, 10, 'unconstrained')
-    profiles(['cobyqa-latest', 'cobyqa'], ['COBYQA Latest', 'COBYQA PyPI'], load=False)
+    profiles(['cobyqa-latest', 'cobyqa'], ['COBYQA Latest', 'COBYQA PyPI'])
