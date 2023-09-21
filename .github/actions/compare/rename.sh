3d73c30d9ea491f7e23bb609e727d1761b3e3546
Tom M. Ragonneau <t.ragonneau@gmail.com>
2023-09-21 09:26:42 +0800

    Correct rename.sh

--- .github/actions/compare/rename.sh
+++ .github/actions/compare/rename.sh
@@ -5,4 +5,4 @@ set -x
 
 HASH=$(git rev-parse --short HEAD)
 find . -type f -exec sed -i "s/cobyqa/cobyqa_$HASH/g" {} +
-find . -type d -exec rename "s/cobyqa/cobyqa_$HASH/" {} ";"
+mv cobyqa "cobyqa_$HASH"
