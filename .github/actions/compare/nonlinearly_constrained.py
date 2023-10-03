397480b532a4d99fd5bdacdc2b39736442608517
Tom M. Ragonneau <33006763+ragonneau@users.noreply.github.com>
2023-10-03 22:18:32 +0800

    Update nonlinearly_constrained.py

--- .github/actions/compare/nonlinearly_constrained.py
+++ .github/actions/compare/nonlinearly_constrained.py
@@ -2,4 +2,4 @@ from profiles import Profiles
 
 if __name__ == '__main__':
     profiles = Profiles(1, 10, 'quadratic other')
-    profiles(['cobyqa-latest', 'cobyqa'], ['COBYQA Latest', 'COBYQA PyPI'])
+    profiles(['cobyqa-latest', 'cobyqa'], ['COBYQA Latest', 'COBYQA PyPI'], [{'radius_final': 0.0}, {'rhoend': 0.0}])
