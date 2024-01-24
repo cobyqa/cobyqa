481dfb788ee6d8794595b9e99208794e092572d9
Tom M. Ragonneau <t.ragonneau@gmail.com>
2024-01-24 17:16:22 +0800

    Remove benchmarks from pyproject.toml

--- .github/actions/compare/setup.sh
+++ .github/actions/compare/setup.sh
@@ -7,21 +7,22 @@ set -x
 find . -type f -maxdepth 1 -exec sed -i "s/cobyqa/cobyqa_latest/g" {} +
 find cobyqa -type f -exec sed -i "s/cobyqa/cobyqa_latest/g" {} +
 mv cobyqa cobyqa_latest
-python -m pip install --progress-bar=off .[benchmarks]
+python -m pip install --progress-bar=off .
 
 # Install cobyqa
 python -m pip install --progress-bar=off cobyqa
 
-# Install optiprofiler
+# Install dependencies
 # TODO: Install optiprofiler properly when available on PyPI.
+python -m pip install --progress-bar=off numpy
 git clone --depth 1 https://github.com/optiprofiler/optiprofiler.git "$GITHUB_WORKSPACE/optiprofiler"
 python -m pip install --progress-bar=off "$GITHUB_WORKSPACE/optiprofiler"
 
 # Download CUTEst and its dependencies
 mkdir "$GITHUB_WORKSPACE/cutest"
-git clone --depth 1 --branch v2.1.28 https://github.com/ralna/ARCHDefs.git "$GITHUB_WORKSPACE/cutest/archdefs"
-git clone --depth 1 --branch v2.0.8 https://github.com/ralna/SIFDecode.git "$GITHUB_WORKSPACE/cutest/sifdecode"
-git clone --depth 1 --branch v2.0.24 https://github.com/ralna/CUTEst.git "$GITHUB_WORKSPACE/cutest/cutest"
+git clone --depth 1 --branch v2.2.3 https://github.com/ralna/ARCHDefs.git "$GITHUB_WORKSPACE/cutest/archdefs"
+git clone --depth 1 --branch v2.1.3 https://github.com/ralna/SIFDecode.git "$GITHUB_WORKSPACE/cutest/sifdecode"
+git clone --depth 1 --branch v2.0.42 https://github.com/ralna/CUTEst.git "$GITHUB_WORKSPACE/cutest/cutest"
 git clone --depth 1 --branch v0.5 https://bitbucket.org/optrove/sif.git "$GITHUB_WORKSPACE/cutest/mastsif"
 
 # Set the environment variables
