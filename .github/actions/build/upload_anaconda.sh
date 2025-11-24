232c29eb78c3508ce3b3d394507ecdf82f8d6080
Tom Ragonneau <33006763+ragonneau@users.noreply.github.com>
2025-11-24 10:09:47 +0100

    Update upload_anaconda.sh

--- .github/actions/build/upload_anaconda.sh
+++ .github/actions/build/upload_anaconda.sh
@@ -7,4 +7,4 @@ export PATH=$CONDA/bin:$PATH
 conda create -n venv -y python="$(python -c 'import platform; print(platform.python_version())')"
 source activate venv
 conda install -y anaconda-client
-anaconda -t "$ANACONDA_TOKEN" upload --force -u cobyqa dist/artifact/*
+anaconda -t "$ANACONDA_TOKEN" upload --force -u cobyqa dist/*
